42681680abd9c7fa0fa7526c56bc7f0ce82340e7
shekharkumargupta@gmail.com <shekharkumargupta@gmail.com@52c72059-4c7c-2ecb-ab4d-334f992a518c>
2012-05-12 03:38:23 +0000

    First Upload

---
+++ trunk/Visitors/src/java/com/opgea/visitors/domain/qualifier/RequestStatusQualifier.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opgea.visitors.domain.qualifier;
+
+/**
+ *
+ * @author Ramesh
+ */
+public enum RequestStatusQualifier {
+
+    NEW_REQUEST, WAIT, CALL, NOT_INTERESTED, CHECK_IN, MEETING_DONE, CHECK_OUT
+}
