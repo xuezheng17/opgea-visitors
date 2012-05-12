ae4538f9efb08e11001a751997af6970defc2811
shekharkumargupta <shekharkumargupta@gmail.com>
2012-05-12 03:38:23 +0000

    First Upload

---
+++ Visitors/src/java/com/opgea/visitors/domain/qualifier/EmployeeType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opgea.visitors.domain.qualifier;
+
+/**
+ *
+ * @author Ramesh
+ */
+public enum EmployeeType {
+    
+    MASTER, EMPLOYEE
+}
---
+++ Visitors/src/java/com/opgea/visitors/domain/qualifier/ReasonOf.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opgea.visitors.domain.qualifier;
+
+/**
+ *
+ * @author Ramesh
+ */
+public enum ReasonOf {
+    LATE_COMING, LATE_SITTING, EARLY_COMMING, EARLY_GOING
+}
