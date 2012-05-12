12a9e362742eed5e3b2968b75cee00600ca9ceb2
shekharkumargupta@gmail.com <shekharkumargupta@gmail.com@52c72059-4c7c-2ecb-ab4d-334f992a518c>
2012-05-12 03:35:50 +0000

    First Upload

---
+++ trunk/Visitors/src/java/com/opgea/constraints/ActionConstraints.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opgea.constraints;
+
+/**
+ *
+ * @author Ramesh
+ */
+public interface ActionConstraints {
+
+    public static final String SUCCESS = "Success";
+    public static final String FAILURE = "Fail";
+}
---
+++ trunk/Visitors/src/java/com/opgea/constraints/SessionConstraints.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opgea.constraints;
+
+/**
+ *
+ * @author Ramesh
+ */
+public enum SessionConstraints {
+    LOGIN_ID, 
+    EMPLOYEE_ID,
+    DEPARTMENT_ID,
+    COMPANY_ID, 
+    COMPANY_NAME, 
+    SESSION_DATA
+    
+}
